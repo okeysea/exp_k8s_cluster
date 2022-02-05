4b118f8cac636941a7ad51ee39aa6ec9994818ef
okeysea <okeysea000@gmail.com>
2022-02-06 01:57:41 +0900

    add initial terraform defines

---
+++ terraform/valiables.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "= 1.1.5"
+
+  required_providers {
+    vultr = {
+      source  = "vultr/vultr"
+      version = "2.9.1"
+    }
+  }
+}
+
+provider "vultr" {
+  api_key     = "${var.CLOUD_VENDOR_API_KEY}"
+  rate_limit  = 700
+  retry_limit = 3
+}
+
+variable "CLOUD_VENDOR_API_KEY" {}
+variable "vultr_region" {}
+variable "k8s_master_plan" {}
+variable "k8s_worker_plan" {}
+variable "k8s_os_id" {}
