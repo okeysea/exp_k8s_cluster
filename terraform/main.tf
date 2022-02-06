041ecedd0792ca5935d57b32b0190a39fb8ec506
okeysea <okeysea000@gmail.com>
2022-02-06 15:05:54 +0900

    add generate ssh config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,16 @@
+locals {
+  ssh_config_file = "/root/.ssh/config"
+}
+
+#### SSH key
+resource "vultr_ssh_key" "k8s_ssh_key" {
+  name    = "k8s_cluster_ssh_key"
+  ssh_key = tls_private_key.keygen.public_key_openssh
+}
+
 #### instances
 
 resource "vultr_instance" "k8s_master" {
-  plan     = var.k8s_master_plan
-  region   = var.vultr_region
-  os_id    = var.k8s_os_id
-  label    = "k8s_master_node"
-  tag      = "k8s master"
-  hostname = "k8s_master_host"
   plan        = var.k8s_master_plan
   region      = var.vultr_region
   os_id       = var.k8s_os_id
@@ -14,4 +18,18 @@ resource "vultr_instance" "k8s_master" {
   tag         = "k8s master"
   hostname    = "k8s_master_host"
   ssh_key_ids = [vultr_ssh_key.k8s_ssh_key.id]
+}
+
+#### make ssh config
+resource "local_file" "ssh_config" {
+  filename = local.ssh_config_file
+  content  = <<-EOF
+  Host ${vultr_instance.k8s_master.label}
+      Hostname ${vultr_instance.k8s_master.main_ip}
+      IdentityFile ${local.private_key_file}
+  EOF
+
+  provisioner "local-exec" {
+    command = "chmod 600 ${local.ssh_config_file}"
+  }
 }
