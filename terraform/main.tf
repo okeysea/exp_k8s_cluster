3f6bff8c54d4c2e47ca7dfb06813e3d51127c262
okeysea <okeysea000@gmail.com>
2022-02-06 15:05:15 +0900

    fix pretty

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,4 +7,11 @@ resource "vultr_instance" "k8s_master" {
   label    = "k8s_master_node"
   tag      = "k8s master"
   hostname = "k8s_master_host"
+  plan        = var.k8s_master_plan
+  region      = var.vultr_region
+  os_id       = var.k8s_os_id
+  label       = "k8s_master_node"
+  tag         = "k8s master"
+  hostname    = "k8s_master_host"
+  ssh_key_ids = [vultr_ssh_key.k8s_ssh_key.id]
 }
